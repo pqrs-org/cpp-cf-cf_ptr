eeafbb54c35e6b2dec352c943a424a7849dca3cd
Takayama Fumihiko <tekezo@pqrs.org>
2022-12-27 00:03:17 +0900

    Update tests to use CFDictionaryRef to avoid the reference count optimization

--- tests/src/test.cpp
+++ tests/src/test.cpp
@@ -6,66 +6,69 @@ int main(void) {
   using namespace boost::ut::literals;
 
   "cf_ptr"_test = [] {
-    auto cfstring1 = CFStringCreateWithCString(kCFAllocatorDefault, "cfstring1", kCFStringEncodingUTF8);
-    auto cfstring2 = CFStringCreateWithCString(kCFAllocatorDefault, "cfstring2", kCFStringEncodingUTF8);
-    expect(CFGetRetainCount(cfstring1) == 1);
-    expect(CFGetRetainCount(cfstring2) == 1);
+    auto cf1 = CFDictionaryCreate(kCFAllocatorDefault, nullptr, nullptr, 0, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
+    auto cf2 = CFDictionaryCreate(kCFAllocatorDefault, nullptr, nullptr, 0, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
+    expect(CFGetRetainCount(cf1) == 1_l);
+    expect(CFGetRetainCount(cf2) == 1_l);
 
     {
-      pqrs::cf::cf_ptr<CFStringRef> ptr1(cfstring1);
-      expect(CFGetRetainCount(cfstring1) == 2);
+      pqrs::cf::cf_ptr<CFDictionaryRef> ptr1(cf1);
+      expect(CFGetRetainCount(cf1) == 2_l);
       expect(ptr1 == true);
 
-      expect(CFGetRetainCount(ptr1.get()) == 2);
-      expect(CFGetRetainCount(*ptr1) == 2);
+      expect(CFGetRetainCount(ptr1.get()) == 2_l);
+      expect(CFGetRetainCount(*ptr1) == 2_l);
 
       ptr1.reset();
-      expect(CFGetRetainCount(cfstring1) == 1);
+      expect(CFGetRetainCount(cf1) == 1_l);
       expect(ptr1 == false);
     }
 
-    expect(CFGetRetainCount(cfstring1) == 1);
+    expect(CFGetRetainCount(cf1) == 1_l);
 
     {
-      pqrs::cf::cf_ptr<CFStringRef> ptr1(cfstring1);
-      expect(CFGetRetainCount(cfstring1) == 2);
+      pqrs::cf::cf_ptr<CFDictionaryRef> ptr1(cf1);
+      expect(CFGetRetainCount(cf1) == 2_l);
 
       ptr1 = nullptr;
-      expect(CFGetRetainCount(cfstring1) == 1);
+      expect(CFGetRetainCount(cf1) == 1_l);
     }
 
     {
-      pqrs::cf::cf_ptr<CFStringRef> ptr1(cfstring1);
-      expect(CFGetRetainCount(cfstring1) == 2);
-      pqrs::cf::cf_ptr<CFStringRef> ptr2(cfstring2);
-      expect(CFGetRetainCount(cfstring2) == 2);
+      pqrs::cf::cf_ptr<CFDictionaryRef> ptr1(cf1);
+      expect(CFGetRetainCount(cf1) == 2_l);
+      pqrs::cf::cf_ptr<CFDictionaryRef> ptr2(cf2);
+      expect(CFGetRetainCount(cf2) == 2_l);
 
       ptr1 = ptr2;
-      expect(CFGetRetainCount(cfstring1) == 1);
-      expect(CFGetRetainCount(cfstring2) == 3);
+      expect(CFGetRetainCount(cf1) == 1_l);
+      expect(CFGetRetainCount(cf2) == 3_l);
 
       ptr1 = nullptr;
-      expect(CFGetRetainCount(cfstring1) == 1);
-      expect(CFGetRetainCount(cfstring2) == 2);
+      expect(CFGetRetainCount(cf1) == 1_l);
+      expect(CFGetRetainCount(cf2) == 2_l);
 
       ptr2 = nullptr;
-      expect(CFGetRetainCount(cfstring1) == 1);
-      expect(CFGetRetainCount(cfstring2) == 1);
+      expect(CFGetRetainCount(cf1) == 1_l);
+      expect(CFGetRetainCount(cf2) == 1_l);
     }
 
     {
-      pqrs::cf::cf_ptr<CFStringRef> ptr1(cfstring1);
-      expect(CFGetRetainCount(cfstring1) == 2);
+      pqrs::cf::cf_ptr<CFDictionaryRef> ptr1(cf1);
+      expect(CFGetRetainCount(cf1) == 2_l);
 
-      pqrs::cf::cf_ptr<CFStringRef> ptr1_1(ptr1);
-      expect(CFGetRetainCount(cfstring1) == 3);
+      pqrs::cf::cf_ptr<CFDictionaryRef> ptr1_1(ptr1);
+      expect(CFGetRetainCount(cf1) == 3_l);
 
       ptr1 = nullptr;
-      expect(CFGetRetainCount(cfstring1) == 2);
+      expect(CFGetRetainCount(cf1) == 2_l);
 
       ptr1_1 = nullptr;
-      expect(CFGetRetainCount(cfstring1) == 1);
+      expect(CFGetRetainCount(cf1) == 1_l);
     }
+
+    CFRelease(cf1);
+    CFRelease(cf2);
   };
 
   return 0;
